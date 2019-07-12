1361147eee91e8bdafe353153623f8155c5871e1
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-12 18:28:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,34 @@
-#introduction
-name="Hi, my name is #{name}."
+def greeting(name, language)
+  puts "Hi my name is #{name} and I am learning tp program in #{language}."
+end
+## Learning Goals
 
-name="Maria"
-#introduction_with_language
- 
- name_language="Hi, my name is #{name} and I am learning to program in #{language}"
- 
- name_language=("Maria,Ruby")
+- Define a method that takes in and uses an argument
+- Define a method that takes in and uses two arguments
+- Define a method with an optional argument
+
+## Introduction
+
+Now that we've seen a few ways of defining methods to add key functionality to
+our programs, let's try writing our own methods. You can use the
+tests to help guide you by running `learn` or `learn test` in your terminal,
+however, this look familiar to you now.
+
+You'll be coding your methods in `lib/introduction.rb`.
+
+## Define a Method That Takes in and Uses an Argument
+
+Define a method, `#introduction` that takes in the argument `name` and outputs
+the phrase: `"Hi, my name is #{name}."`
+
+## Define a Method That Takes in and Uses Two Arguments
+
+Define a method, `#introduction_with_language` that takes in two arguments,
+`name` and `language` and outputs the phrase: "Hi, my name is #{name} and I am
+learning to program in #{language}."
+
+## Define a Method With an Optional Argument
+
+Copy the previous method and name it `#introduction_with_language_optional`. It
+that takes in two arguments,`name` and `language`, but language is optional. The
+default value for `language` will be "Ruby".
